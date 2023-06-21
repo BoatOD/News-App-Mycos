d7fe29e929bac4fc8e593658db9405a59fd65ed7
KENTHN658 <118724637+KENTHN658@users.noreply.github.com>
2023-06-21 19:41:01 +0700

    search

--- src/views/Search.tsx
+++ src/views/Search.tsx
@@ -1,7 +1,31 @@
-const Search = () => {
+import React, { useState } from 'react';
+import { TextInput } from '@mantine/core';
+
+function Search() {
+  const [value, setValue] = useState('');
+  
+  const handleSearch = () => {
+    const test = `https://newsapi.org/v2/everything?q=${value}&apiKey=3fa13ca565844619bc31559ca018df2f`;
+    
+    // Perform the fetch request here
+    fetch(test)
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the fetched data
+        console.log(data);
+      })
+      .catch((error) => {
+        // Handle any errors
+        console.error(error);
+      });
+  };
+  
   return (
-    <div>Search</div>
-  )
+    <>
+      <TextInput value={value} onChange={(event) => setValue(event.currentTarget.value)} />
+      <button type='button' onClick={handleSearch}>Search</button>
+    </>
+  );
 }
 
-export default Search
+export default Search;
